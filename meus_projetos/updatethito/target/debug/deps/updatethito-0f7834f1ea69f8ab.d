b02d883459f9651ccc340d6dcc1aaf77c49a5381
codigosdothi <iamthisoares@outlook.com>
2026-08-14 08:52:51 -0300

    feat: Injeção do God Mode v2 finalizada e testada

---
+++ meus_projetos/updatethito/target/debug/deps/updatethito-0f7834f1ea69f8ab.d
@@ -0,0 +1,5 @@
+C:\Users\Thiago Soares\treinamento_rust\meus_projetos\updatethito\target\debug\deps\updatethito-0f7834f1ea69f8ab.d: src\main.rs
+
+C:\Users\Thiago Soares\treinamento_rust\meus_projetos\updatethito\target\debug\deps\libupdatethito-0f7834f1ea69f8ab.rmeta: src\main.rs
+
+src\main.rs:
---
+++ meus_projetos/updatethito/target/debug/deps/updatethito-4032b16aede47975.d
@@ -0,0 +1,5 @@
+C:\Users\Thiago Soares\treinamento_rust\meus_projetos\updatethito\target\debug\deps\updatethito-4032b16aede47975.d: src\main.rs
+
+C:\Users\Thiago Soares\treinamento_rust\meus_projetos\updatethito\target\debug\deps\updatethito-4032b16aede47975.exe: src\main.rs
+
+src\main.rs:
